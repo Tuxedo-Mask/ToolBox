2853708556bdaf37885e0a8cdffb616c13b07fac
Hayk <haykhakobyan89@gmail.com>
2020-05-05 23:26:17 +0400

    Added sceleton server based on express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Welcome to the real world!');
+});
+
+const PORT = 3000;
+app.listen(PORT, function () {
+    console.log(`Server is listening on port ${PORT}!`);
+});
