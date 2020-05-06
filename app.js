f34709ff2d05cc94bd64e4014c3182f44305b16f
Hayk <haykhakobyan89@gmail.com>
2020-05-06 21:42:52 +0400

    Added mysql testing code

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const express = require('express');
+const express = require("express");
+const config = require("./config");
 const app = express();
+
+const mysql = require("mysql");
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "testUser",
+    password: "Password1",
+    database: "testscheme"
+});
+
+// HH::TODO Add module to set up db connection, create test schemes and table, fill test data
+
+con.connect(function(err) {
+    if (err) {
+        throw err;
+    }
+    con.query("SELECT * FROM testscheme.users", function (err, result, fields) {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+    });
+});
+
+// HH::TODO check if call to connect blocks event loop;
 
 app.get('/', function (req, res) {
     res.send('Welcome to the real world!');
 });
 
-const PORT = 3000;
-const MYSQL_PORT = 3306;
-const X_PROTOCOL_PORT = 33060;
-app.listen(PORT, function () {
-    console.log(`Server is listening on port ${PORT}!`);
+app.listen(config.PORT, function () {
+    console.log(`Server is listening on port ${config.PORT}!`);
 });
