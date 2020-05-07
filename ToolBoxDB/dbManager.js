2d42fde303207c76bda06f2c5e3a5da84943ea1c
Chimera <haykhakobyan89@gmail.com>
2020-05-07 14:21:10 +0400

    Added dbManager module and some test commands

---
+++ ToolBoxDB/dbManager.js
@@ -0,0 +1,54 @@
+module.exports = () => {
+    return {
+        CreateTestScheme: (con) => {
+            const createTestDBScript = "CREATE DATABASE IF NOT EXISTS testDB";
+            con.query(createTestDBScript, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log("Database testDB created");
+            });
+        },
+        CreateTestTable: (con) => {
+            const createTestUsersTableScript = "CREATE TABLE IF NOT EXISTS TestUsers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), lastName VARCHAR(255))";
+
+            con.query(createTestUsersTableScript, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log("Created Table TestUsers");
+            });
+        },
+        DeleteAllData: (con) => {
+            var clear = "DELETE FROM TestUsers WHERE id > 0";
+
+            con.query(clear, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log("Cleared TestUsers table");
+            });
+        },
+        CreateTestData: (con) => {
+            var createTestUsersDataScript = "INSERT INTO TestUsers (name, lastName) VALUES ('Hayk', 'Hakobyan')";
+
+            con.query(createTestUsersDataScript, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log("Added TestUsers Data");
+            });
+        },
+        SelecAll: (con) => {
+            const selectAll = "SELECT id, name, lastName FROM TestUsers WHERE id > 0";
+
+            con.query(selectAll, function (err, result) {
+                if (err) {
+                    throw err;
+                }
+                console.log("Selected All");
+                console.log(result);
+            });
+        }
+    };
+}
--- app.js
+++ app.js
@@ -1,31 +1,45 @@
 const express = require("express");
 const config = require("./config");
 const app = express();
+const dbManager = require("./ToolBoxDB/dbManager")();
 
 const mysql = require("mysql");
 var con = mysql.createConnection({
     host: "localhost",
     user: "testUser",
-    password: "Password1",
-    database: "testscheme"
+    password: "Password1"
 });
-
-// HH::TODO Add module to set up db connection, create test schemes and table, fill test data
 
 con.connect(function(err) {
     if (err) {
         throw err;
     }
-    con.query("SELECT * FROM testscheme.users", function (err, result, fields) {
-        if (err) {
-            throw err;
-        }
-        console.log(result);
-    });
+    console.log("Connected to mysql DB!");
+
+    dbManager.CreateTestScheme(con)
+});
+con.end();
+
+// HH::TODO find more elegant solution
+con =  mysql.createConnection({
+    host: "localhost",
+    user: "testUser",
+    password: "Password1",
+    database: "testDB"
+});
+con.connect(function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected to mysql DB!");
+
+    dbManager.CreateTestTable(con);
+    dbManager.DeleteAllData(con);
+    dbManager.CreateTestData(con);
+    dbManager.SelecAll(con);
 });
 
 // HH::TODO check if call to connect blocks event loop;
-
 app.get('/', function (req, res) {
     res.send('Welcome to the real world!');
 });
