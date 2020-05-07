974c1975aed3fbb1e8a6589d191b4b691e623d22
Hayk <haykhakobyan89@gmail.com>
2020-05-08 02:33:03 +0400

    Moved db related  logic into db mgr

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-'use strict';
+"use strict";
+
+require('dotenv').config();
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === environments.dev) {
   // eslint-disable-next-line global-require
@@ -25,45 +27,11 @@ process.on('unhandledRejection', err => uncaughtErrorHandler('unhandledRejection
 const express = require("express");
 const config = require("./config");
 const app = express();
+
 const dbManager = require("./src/dbManager")();
+// HH::TODO call to dbManager.Test() blocks the event loop, find solution using connection pool 
+dbManager.Test();
 
-const mysql = require("mysql");
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "testUser",
-  password: "Password1"
-});
-
-con.connect(function(err) {
-  if (err) {
-    throw err;
-  }
-  console.log("Connected to mysql DB!");
-
-  dbManager.CreateTestScheme(con)
-});
-con.end();
-
-// HH::TODO find more elegant solution
-con = mysql.createConnection({
-  host: "localhost",
-  user: "testUser",
-  password: "Password1",
-  database: "testDB"
-});
-con.connect(function(err) {
-  if (err) {
-    throw err;
-  }
-  console.log("Connected to mysql DB!");
-
-  dbManager.CreateTestTable(con);
-  dbManager.DeleteAllData(con);
-  dbManager.CreateTestData(con);
-  dbManager.SelectAll(con);
-});
-
-// HH::TODO check if call to connect blocks event loop;
 app.get('/', function (req, res) {
   res.send('Welcome to the real world!');
 });
--- src/dbManager/dbManager.js
+++ src/dbManager/dbManager.js
@@ -1,54 +1,105 @@
+const { dbCredentials } = require('../config');
+const mysql = require("mysql");
+// HH::TODO const log = require('../logger');
+
+/**
+ *
+ * @param {object} conn - db connection object
+ * @param {string} sql - query
+ * @param guid - Id used inside logs
+ * @returns {Promise<*>}
+ * @private
+ */
+const Query = async function query(conn, sql, guid = null) {
+  //HH::TODO log.info(`SQL: ${sql}`, guid);
+  return new Promise((resolve, reject) => {
+    conn.query({ sql, timeout: 1000 }, (error, result) => {
+      // HH::TODO add db configconn.query({sql, timeout: dbConfiguration.queryTimeout }, (error, result) => {
+      if (error) {
+        error.message = `sql query failed: ${error.message}`;
+        //HH::TODO log.error(error.message, guid);
+        return reject(error);
+      }
+      resolve(result);
+    });
+  });
+}
+
+const CreateTestScheme = (con) => {
+  const createTestDBScript = "CREATE DATABASE IF NOT EXISTS testDB";
+  Query(con, createTestDBScript);
+  console.log("Database testDB created");
+};
+
+const CreateTestTable = (con) => {
+  const createTestUsersTableScript = "CREATE TABLE IF NOT EXISTS TestUsers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), lastName VARCHAR(255))";
+  Query(con, createTestUsersTableScript);
+  console.log("Created Table TestUsers");
+};
+
+const DeleteAllData = (con) => {
+  const clear = "DELETE FROM TestUsers WHERE id > 0";
+  Query(con, clear);
+  console.log("Cleared TestUsers table");
+};
+
+const CreateTestData = (con) => {
+  const createTestUsersDataScript = "INSERT INTO TestUsers (name, lastName) VALUES ('Hayk', 'Hakobyan')";
+  Query(con, createTestUsersDataScript);
+  console.log("Added TestUsers Data");
+};
+
+const SelectAll = (con) => {
+  const selectAll = "SELECT id, name, lastName FROM TestUsers WHERE id > 0";
+  const promise = Query(con, selectAll);
+  promise.then(
+    result => console.log(result),
+    error => console.log(error)
+  );
+  console.log("Selected All");
+};
+
 module.exports = () => {
   return {
-    CreateTestScheme: (con) => {
+    Query: Query,
+    CreateTestScheme: CreateTestScheme,
+    CreateTestTable: CreateTestTable,
+    DeleteAllData: DeleteAllData,
+    CreateTestData: CreateTestData,
+    SelectAll: SelectAll,
+    Test: () => {
+      console.log("Testing DB");
+
+      const mysql = require("mysql");
+      console.log(dbCredentials.user);
+      const con = mysql.createConnection({
+        host: dbCredentials.host,
+        user: dbCredentials.user,
+        password: dbCredentials.password
+      });
+
       const createTestDBScript = "CREATE DATABASE IF NOT EXISTS testDB";
-      con.query(createTestDBScript, function (err, result) {
-        if (err) {
-          throw err;
-        }
-        console.log("Database testDB created");
-      });
-    },
-    CreateTestTable: (con) => {
-      const createTestUsersTableScript = "CREATE TABLE IF NOT EXISTS TestUsers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), lastName VARCHAR(255))";
+      Query(con, createTestDBScript).then(
+        result => {
+          con1 = mysql.createConnection({
+            host: dbCredentials.host,
+            user: dbCredentials.user,
+            password: dbCredentials.password,
+            database: dbCredentials.database
+          });
+          con1.connect(function (err) {
+            if (err) {
+              throw err;
+            }
+            console.log("Connected to mysql DB!");
 
-      con.query(createTestUsersTableScript, function (err, result) {
-        if (err) {
-          throw err;
+            CreateTestTable(con1);
+            DeleteAllData(con1);
+            CreateTestData(con1);
+            SelectAll(con1);
+          });
         }
-        console.log("Created Table TestUsers");
-      });
-    },
-    DeleteAllData: (con) => {
-      var clear = "DELETE FROM TestUsers WHERE id > 0";
-
-      con.query(clear, function (err, result) {
-        if (err) {
-          throw err;
-        }
-        console.log("Cleared TestUsers table");
-      });
-    },
-    CreateTestData: (con) => {
-      var createTestUsersDataScript = "INSERT INTO TestUsers (name, lastName) VALUES ('Hayk', 'Hakobyan')";
-
-      con.query(createTestUsersDataScript, function (err, result) {
-        if (err) {
-          throw err;
-        }
-        console.log("Added TestUsers Data");
-      });
-    },
-    SelectAll: (con) => {
-      const selectAll = "SELECT id, name, lastName FROM TestUsers WHERE id > 0";
-
-      con.query(selectAll, function (err, result) {
-        if (err) {
-          throw err;
-        }
-        console.log("Selected All");
-        console.log(result);
-      });
+      );
     }
   };
 }
