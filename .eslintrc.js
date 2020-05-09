4aa6b2049f688bac3ce12a33f8fd6bf185c8abd0
Tuxedo-Mask <haykhakobyan89@gmail.com>
2020-05-09 14:38:50 +0400

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	"extends": "standard",
+	"rules": {
+		"semi": ["error", "always"],
+		"key-spacing": ["error", {
+			"mode": "minimum"
+		}],
+		"no-trailing-spaces": ["error", {
+			"ignoreComments": true
+		}],
+		"valid-typeof": [
+			"error",
+			{
+				"requireStringLiterals": false
+			}
+		],
+		"max-len": ["error", { "code": 160, "tabWidth": 4, "ignoreComments": true }],
+		"indent": ["error", 4],
+		"quotes": ["error", "double"],
+		"no-console": "error",
+		"global-require": "error",
+		"no-duplicate-imports": "error",
+		"array-callback-return": "error",
+		"object-curly-spacing": "off",
+		"camelcase": "error",
+		"no-prototype-builtins": "off",
+		"no-undef": "off"
+	}
+};
