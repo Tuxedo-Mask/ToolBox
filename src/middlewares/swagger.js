73ec92aa156beb3101a34f13703a7d58548d4f59
Vahagn <Mr.Zohrabyan@users.noreply.github.com>
2020-05-12 17:33:23 +0400

    chage swagger 2.0 to open api 3.0.1

--- src/middlewares/swagger.js
+++ src/middlewares/swagger.js
@@ -1,4 +1,4 @@
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 
-module.exports = [swaggerUI.serve, swaggerUI.setup(YAML.load("./api/swagger.yaml"))];
+module.exports = [swaggerUI.serve, swaggerUI.setup(YAML.load("./api/openapi.yaml"))];
