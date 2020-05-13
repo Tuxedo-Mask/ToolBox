5a1a3c987fdac20c0cc5a505d63a9f6f9872e26b
Hayk <haykhakobyan89@gmail.com>
2020-05-13 23:54:57 +0400

    Cleaned up dbManager and fixed blocking behaviour, fixed some in-code Todos and added new ones

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
+
 const constants = require("./src/constants");
+const logger = require("./src/logger")();
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === constants.environments.dev) {
   // eslint-disable-next-line global-require
@@ -7,24 +9,24 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === constants.environments.dev
 }
 
 const config = require("./src/config");
-const logger = require("./src/logger")();
-logger.info(`Running in '${config.nodeEnv}' environment`);
-// VZ::TODO delete
-logger.info(`logLevel: ${config.logLevel}`);
+logger.info(`Running in "${config.nodeEnv}" environment`);
+
+// HH::TODO move dbManager testing part to unit testing
+const dbManager = require("./src/dbManager")();
+dbManager.Test();
 
 const attachMiddlewares = require("./src/middlewares");
 const { uncaughtErrorHandler } = require("./src/utils");
+
+// HH::TODO are this Handlers on correct lines? VZ::Please take a look
+// Checking Uncaught Exceptions and Unhandled Rejections
+process.on("uncaughtException", err => uncaughtErrorHandler("uncaughtException", err));
+process.on("unhandledRejection", err => uncaughtErrorHandler("unhandledRejection", err));
+
 const app = require("express")();
 // Attach express middlewares
 attachMiddlewares(app);
 
-// const dbManager = require("./src/dbManager")();
-// HH::TODO call to dbManager.Test() blocks the event loop, find solution using connection pool 
-// dbManager.Test();
 app.listen(config.port, function () {
   logger.info(`Server is listening on port ${config.port}!`);
 });
-
-// Checking Uncaught Exceptions and Unhandled Rejections
-process.on("uncaughtException", err => uncaughtErrorHandler("uncaughtException", err));
-process.on("unhandledRejection", err => uncaughtErrorHandler("unhandledRejection", err));
--- src/dbManager/dbManager.js
+++ src/dbManager/dbManager.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const { dbCredentials } = require("../config");
 const mysql = require("mysql");
 const logger = require("../logger")();
@@ -14,7 +16,6 @@ const Query = async function query (conn, sql, guid = null) {
   logger.info(`SQL: ${sql}`, guid);
   return new Promise((resolve, reject) => {
     conn.query({ sql, timeout: 1000 }, (error, result) => {
-      // HH::TODO add db configconn.query({sql, timeout: dbConfiguration.queryTimeout }, (error, result) => {
       if (error) {
         error.message = `sql query failed: ${error.message}`;
         logger.error(error.message, guid);
@@ -25,52 +26,13 @@ const Query = async function query (conn, sql, guid = null) {
   });
 };
 
-const CreateTestScheme = async (con) => {
-  const createTestDBScript = "CREATE DATABASE IF NOT EXISTS testDB";
-  await Query(con, createTestDBScript);
-  logger.info("Database testDB created");
-};
-
-const CreateTestTable = async (con) => {
-  // eslint-disable-next-line max-len
-  const createTestUsersTableScript = "CREATE TABLE IF NOT EXISTS TestUsers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), lastName VARCHAR(255))";
-  await Query(con, createTestUsersTableScript);
-  logger.info("Created Table TestUsers");
-};
-
-const DeleteAllData = async (con) => {
-  const clear = "DELETE FROM TestUsers WHERE id > 0";
-  await Query(con, clear);
-  logger.info("Cleared TestUsers table");
-};
-
-const CreateTestData = async (con) => {
-  const createTestUsersDataScript = "INSERT INTO TestUsers (name, lastName) VALUES ('Hayk', 'Hakobyan')";
-  await Query(con, createTestUsersDataScript);
-  logger.info("Added TestUsers Data");
-};
-
-const SelectAll = (con) => {
-  const selectAll = "SELECT id, name, lastName FROM TestUsers WHERE id > 0";
-  const promise = Query(con, selectAll);
-  promise.then(
-    result => logger.info(result),
-    error => logger.info(error)
-  );
-  logger.info("Selected All");
-};
-
 module.exports = () => {
   return {
     Query: Query,
-    CreateTestScheme: CreateTestScheme,
-    CreateTestTable: CreateTestTable,
-    DeleteAllData: DeleteAllData,
-    CreateTestData: CreateTestData,
-    SelectAll: SelectAll,
     Test: async () => {
       logger.info("Testing DB");
 
+      // HH::TODO why this const does works? VZ::Please take a look
       const con = mysql.createConnection({
         host: dbCredentials.host,
         user: dbCredentials.user,
@@ -78,27 +40,24 @@ module.exports = () => {
       });
 
       const createTestDBScript = "CREATE DATABASE IF NOT EXISTS testDB";
-      Query(con, createTestDBScript).then(
-        result => {
-          con1 = mysql.createConnection({
-            host: dbCredentials.host,
-            user: dbCredentials.user,
-            password: dbCredentials.password,
-            database: dbCredentials.database
-          });
-          con1.connect((err) => {
-            if (err) {
-              throw err;
-            }
-            logger.info("Connected to mysql DB!");
-
-            CreateTestTable(con1);
-            DeleteAllData(con1);
-            CreateTestData(con1);
-            SelectAll(con1);
-          });
+      await Query(con, createTestDBScript);
+      con.changeUser({database: dbCredentials.database}, function (err) {
+        if (err) {
+          throw err;
         }
-      );
+      });
+      // eslint-disable-next-line max-len
+      const createTestUsersTableScript = "CREATE TABLE IF NOT EXISTS TestUsers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), lastName VARCHAR(255))";
+      await Query(con, createTestUsersTableScript);
+      const clear = "DELETE FROM TestUsers WHERE id > 0";
+      await Query(con, clear);
+      const createTestUsersDataScript = "INSERT INTO TestUsers (name, lastName) VALUES ('Hayk', 'Hakobyan')";
+      await Query(con, createTestUsersDataScript);
+      const selectAll = "SELECT id, name, lastName FROM TestUsers WHERE id > 0";
+      await Query(con, selectAll).then(result => {
+        logger.info(JSON.stringify(result));
+      });
+      con.end();
     }
   };
 };
