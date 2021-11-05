3675ac62e6ea427e317d667bb84cc65424a6dca7
Chimera <haykhakobyan89@gmail.com>
2021-11-05 23:27:49 +0400

    Added clearDb script into package.json

---
+++ src/db/clearDb.js
@@ -0,0 +1,18 @@
+const constants = require('../constants');
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === constants.environments.dev) {
+  // eslint-disable-next-line global-require
+  require('dotenv').config('ToolBox');
+}
+const config = require('../config');
+
+const dbManager = require('../dbManager')(config.dbCredentials.connection);
+try {
+  (async () => {
+    await dbManager.knex().raw(`DROP DATABASE IF EXISTS ${config.dbCredentials.database};`);
+    await dbManager.knex().destroy();
+  })();
+} catch (err) {
+  console.log('Exception occurred during migrations, err: ', err);
+  dbManager.knex().destroy();
+}
