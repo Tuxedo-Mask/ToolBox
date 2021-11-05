e7f8e3287474a8b83b07ca0986b636adecddd17e
Chimera <haykhakobyan89@gmail.com>
2021-11-05 23:21:40 +0400

    Added new columns to tables and foreign keys, updated TODO list

--- src/db/version1.js
+++ src/db/version1.js
@@ -8,8 +8,8 @@ module.exports = async () => {
     await dbManager.knex().schema.withSchema(config.dbCredentials.database)
         .createTable('users', (table) => {
           table.increments();
-          table.string('name');
-          table.string('lastname');
+          table.string('name').notNullable();
+          table.string('lastname').notNullable();
         });
   }
   const storageRoom = await dbManager.knex().schema.withSchema(config.dbCredentials.database).hasTable('storageRoom');
@@ -17,9 +17,17 @@ module.exports = async () => {
     await dbManager.knex().schema.withSchema(config.dbCredentials.database)
         .createTable('storageRoom', (table) => {
           table.increments();
-          table.string('name');
-          table.string('address');
-          table.string('description');
+          table.integer('ownerId', 10).unsigned();
+          table.string('name').notNullable();
+          table.string('address').notNullable();
+          table.string('description').nullable();
+          table.string('imagePath').nullable();
+          table.unique(['name', 'address']);
+          table.foreign('ownerId')
+              .references('id')
+              .inTable('users')
+              .onDelete('CASCADE')
+              .onUpdate('CASCADE');
         });
   }
   const storage = await dbManager.knex().schema.withSchema(config.dbCredentials.database).hasTable('storage');
@@ -27,9 +35,16 @@ module.exports = async () => {
     await dbManager.knex().schema.withSchema(config.dbCredentials.database)
         .createTable('storage', (table) => {
           table.increments();
-          table.string('name');
-          table.integer('typeId');
-          table.integer('categoryId');
+          table.string('name').notNullable();
+          table.integer('storageRoomId', 10).unsigned();
+          table.integer('typeId').nullable();
+          table.integer('categoryId').nullable();
+          table.string('imagePath').nullable();
+          table.foreign('storageRoomId')
+              .references('id')
+              .inTable('storageRoom')
+              .onDelete('CASCADE')
+              .onUpdate('CASCADE');
         });
   }
 };
